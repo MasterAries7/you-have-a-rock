d9bff547f222c98dd1f1044b7594a45038bbf923
MasterAries7 <masteraries7@gmail.com>
2017-01-03 12:39:40 -0600

    Learning and Experimentation
    Learning about Buttons, and how to control things with JS and CSS

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,18 @@
 ﻿var timeInterval = 1000 / 40
-var count = 0
+var frame_count = 0
+
 
 function gameLoop() {
-    document.getElementById("test1").innerHTML = count
-    count++
+    document.getElementById("frame-counter").innerHTML = (frame_count % 40) + 1
+    frame_count++
+}
 
+function lookAround() {
+    if (Math.random() < .75) {
+        document.getElementById("console").innerHTML = "You look around for a while, but find nothing interesting."
+    } else {
+        document.getElementById("console").innerHTML = "You look around for a while, and find another rock."
+    }
 }
 
 setInterval(gameLoop,timeInterval)
